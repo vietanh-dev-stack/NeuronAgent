7784fcebf6cd1f76b99a7b26f9f38139e268c9de
Viet Anh <163091992+vietanh-dev-stack@users.noreply.github.com>
2026-04-15 22:27:44 +0700

    load model AI

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,35 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "NeuroAgent backend is running 🚀"}
+# Model request
+class ChatRequest(BaseModel):
+    message: str
+
+model_name = "microsoft/DialoGPT-medium"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+def generate_response(prompt):
+    inputs = tokenizer(prompt, return_tensors="pt")
+    
+    outputs = model.generate(
+        **inputs,
+        max_new_tokens=100,
+        do_sample=True,
+        temperature=0.7
+    )
+
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+@app.post("/chat")
+async def chat(data: ChatRequest):
+    user_input = data.message
+    
+    response = generate_response(user_input)
+    
+    return {"response": response}
